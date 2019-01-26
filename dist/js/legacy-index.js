c34f829683a3d22047c3391660f41c1c8d1da968
RXstoinkboy <erykludwin@gmail.com>
2019-01-26 10:03:29 +0100

    added link highlighter

--- dist/js/legacy-index.js
+++ dist/js/legacy-index.js
@@ -141,12 +141,15 @@ var _darkMode = __webpack_require__(/*! ./darkMode.js */ "./src/js/darkMode.js")
 
 var _returnHome = __webpack_require__(/*! ./returnHome.js */ "./src/js/returnHome.js");
 
+// import { highlight } from './highlight.js';
 window.addEventListener('DOMContentLoaded', function () {
   var buttonDarkMode = document.querySelector('.button--toggleDarkMode');
   var buttonSizeDown = document.querySelector('.button--fontSizeDown');
   var buttonSizeUp = document.querySelector('.button--fontSizeUp');
   var buttonToTop = document.querySelector('.button--returnHome');
-  var current = 18;
+  var menuItems = document.querySelectorAll('.nav-link');
+  var highlighter = document.createElement('div');
+  var current = 18; // change font size
 
   function changeSize(e) {
     if (e.target.classList == 'button button--fontSizeUp') {
@@ -156,12 +159,41 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     document.documentElement.style.setProperty('--fontSize', current + "px");
+  } // highlighter for menu items
+
+
+  highlighter.classList.add('menuSelector');
+  document.body.appendChild(highlighter);
+
+  function highlight() {
+    var dimensions = this.getBoundingClientRect();
+    var data = {
+      width: dimensions.width,
+      height: dimensions.height,
+      left: dimensions.left,
+      top: dimensions.top
+    };
+    highlighter.style.width = data.width + "px";
+    highlighter.style.height = data.height + "px";
+    highlighter.style.top = window.scrollY + data.top + "px";
+    highlighter.style.left = window.scrollX + data.left + "px";
   }
 
   buttonSizeDown.addEventListener('click', changeSize);
   buttonSizeUp.addEventListener('click', changeSize);
   buttonToTop.addEventListener('click', _returnHome.returnHome);
   buttonDarkMode.addEventListener('click', _darkMode.toggleDarkMode);
+  menuItems.forEach(function (item) {
+    return item.addEventListener('mouseenter', highlight);
+  }); // do not display highlighter on mobiles
+
+  window.addEventListener('resize', function () {
+    if (window.innerWidth < 800) {
+      highlighter.style.display = 'none';
+    } else {
+      highlighter.style.display = 'block';
+    }
+  });
 });
 
 /***/ }),
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,18 @@
 import { toggleDarkMode } from './darkMode.js';
 import { returnHome } from './returnHome.js';
+// import { highlight } from './highlight.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   const buttonDarkMode = document.querySelector('.button--toggleDarkMode');
   const buttonSizeDown = document.querySelector('.button--fontSizeDown');
   const buttonSizeUp = document.querySelector('.button--fontSizeUp');
   const buttonToTop = document.querySelector('.button--returnHome');
+  const menuItems = document.querySelectorAll('.nav-link');
+  const highlighter = document.createElement('div');
 
   let current = 18;
 
+  // change font size
   function changeSize(e) {
     if (e.target.classList == 'button button--fontSizeUp') {
       current++;
@@ -18,8 +22,38 @@ window.addEventListener('DOMContentLoaded', () => {
     document.documentElement.style.setProperty('--fontSize', `${current}px`);
   }
 
+  // highlighter for menu items
+  highlighter.classList.add('menuSelector');
+  document.body.appendChild(highlighter);
+
+  function highlight() {
+    const dimensions = this.getBoundingClientRect();
+
+    const data = {
+      width: dimensions.width,
+      height: dimensions.height,
+      left: dimensions.left,
+      top: dimensions.top,
+    };
+
+    highlighter.style.width = `${data.width}px`;
+    highlighter.style.height = `${data.height}px`;
+    highlighter.style.top = `${window.scrollY + data.top}px`;
+    highlighter.style.left = `${window.scrollX + data.left}px`;
+  }
+
   buttonSizeDown.addEventListener('click', changeSize);
   buttonSizeUp.addEventListener('click', changeSize);
   buttonToTop.addEventListener('click', returnHome);
   buttonDarkMode.addEventListener('click', toggleDarkMode);
+  menuItems.forEach(item => item.addEventListener('mouseenter', highlight));
+
+  // do not display highlighter on mobiles
+  window.addEventListener('resize', () => {
+    if (window.innerWidth < 800) {
+      highlighter.style.display = 'none';
+    } else {
+      highlighter.style.display = 'block';
+    }
+  });
 });
